5098fd0ab075dd36054cdbd670b00b289fc62611
AndersonV1 <166729177+AndersonV1@users.noreply.github.com>
2024-04-16 08:21:07 -0400

    practicas de clase diseno web2

---
+++ Java Script/js/javascript00.js
@@ -0,0 +1,13 @@
+//comentarios de una line 
+/*
+para hacer comentarios 
+de multiples 
+lineas
+*/    
+/*
+Descripcion:ejemplo de Hola mundo 
+Desarrollador:Nombre y Apellido
+fecha:2024-04-15
+cambios:ninguno
+*/
+console.log("Hola mundo,se vera");
