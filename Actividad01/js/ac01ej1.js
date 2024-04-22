f91865cd96b12081524d0dc977d0143e34e81b63
AndersonV1 <166729177+AndersonV1@users.noreply.github.com>
2024-04-22 08:03:29 -0400

    actividad

---
+++ Actividad01/js/ac01ej1.js
@@ -0,0 +1,12 @@
+/*
+Descripcion: Actividad01 Ejercicio1 
+Desarrollador: Anderson Vargas Saucedo
+Fecha: 2024-04-20
+Cambios: Ninguno
+*/
+
+
+var mensaje="Bienvenido(a)";
+var mensaje1="Estas abriendo con Navegador Firefox";
+console.log(mensaje);
+console.log(mensaje1);
